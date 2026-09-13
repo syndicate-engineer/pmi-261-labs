2695544156bbb05193a609bf3670af01c2cc6cae
Syndicate Engineer <syndicate_engineer@icloud.com>
2026-09-13 22:27:51 +0700

    загрузил решения задач из лабораторной работы №1

---
+++ labs/lab1/task1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Четверостишье из трека Octavarium - Dream Theater
+    std::cout << "We move in circles, balanced all the while\n"
+                 "On a gleaming razor's edge\n"
+                 "A perfect sphere colliding with our fate\n"
+                 "This story ends where it began!\n";
+    return 0;
+}
